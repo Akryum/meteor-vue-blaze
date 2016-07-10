f18ee94ed827aa391f8c3d222a75384c2a6949ac
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-07-10 20:39:21 +0200

    Embed Blaze template support

--- imports/ui/Widget.vue
+++ imports/ui/Widget.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="widget">
+    <h2>This is a Vue component</h2>
     <div>Hello {{name}}!</div>
     <input v-model="name" placeholder="Enter your name" />
     <div>
       You've pressed the button {{counter}} times (inside vue component).
     </div>
+
+    <hr />
+
+    <div>
+      <button @click="toggleShowBlaze">Toggle embed Blaze template</button>
+      <button @click="toggleBlazeTemplate">Switch template</button>
+    </div>
+
+    <div class="blaze-template" v-if="showBlaze" v-blaze="blazeTemplate"></div>
   </div>
 </template>
 
@@ -15,7 +25,9 @@ export default {
   data() {
     return {
       name: 'world',
-      counter: 0
+      counter: 0,
+      blazeTemplate: 'hello',
+      showBlaze: true
     }
   },
   meteor: {
@@ -23,6 +35,18 @@ export default {
       counter() {
         return Session.get("counter");
       }
+    }
+  },
+  methods: {
+    toggleBlazeTemplate() {
+      if(this.blazeTemplate === 'hello') {
+        this.blazeTemplate = 'loginButtons';
+      } else {
+        this.blazeTemplate = 'hello';
+      }
+    },
+    toggleShowBlaze() {
+      this.showBlaze = !this.showBlaze;
     }
   }
 }
@@ -37,6 +61,12 @@ export default {
   border-bottom: solid 1px #40b883;
 }
 
+h2 {
+  margin: 0 0 12px 0;
+  font-weight: normal;
+  text-align: center;
+}
+
 input {
   display: block;
   margin: 4px 0;
@@ -44,6 +74,13 @@ input {
   box-sizing: border-box;
   border: none;
   padding: 6px 12px;
+  border-radius: 3px;
+}
+
+.blaze-template {
+  margin-top: 12px;
+  padding: 12px;
+  background: white;
   border-radius: 3px;
 }
 </style>
